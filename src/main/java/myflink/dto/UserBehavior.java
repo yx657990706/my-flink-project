d0f0c61e311ddc2b5d82c144f73f5db171bb5841
yinxiong <yinxiong>
2020-02-23 21:26:03 +0800

    init

---
+++ src/main/java/myflink/dto/UserBehavior.java
@@ -0,0 +1,9 @@
+package myflink.dto;
+
+public class UserBehavior {
+    public long userId;         // 用户ID
+    public long itemId;         // 商品ID
+    public int categoryId;      // 商品类目ID
+    public String behavior;     // 用户行为, 包括("pv", "buy", "cart", "fav")
+    public long timestamp;      // 行为发生的时间戳，单位秒
+}
